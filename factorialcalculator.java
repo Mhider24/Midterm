4b71bba043b7a94d407f8f8a1a8caddedb9736f1
Mohammad Hider <mhider2006@gmail.com>
2025-02-27 14:56:58 -0500

    Changed num and added a comment

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,6 +1,6 @@
-public class factorialcalculator {//This is the repeated change for question 2
+public class factorialcalculator {//This is the factorial class
     public static void main(String[] args) {
-        int num = 5;  // Predefined number to calculate factorial
+        int num = 7;  // Predefined number to calculate factorial
         int factorial = 1;
 
         for (int i = 1; i <= num; i++) { // Loop through numbers
